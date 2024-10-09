e45a0d219f117af29feeeabd8e286ed54475b416
adam tarango <you@example.comadam.tarango97@gmail.comgit commitgit statusgit statusgit config --global user.email you@example.comadam.tarango97@gmail.com>
2024-10-09 09:16:32 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const message =  `You have received this message because you have been chosen to open an important vault. Here is the secret combination:`
+alert(message)
+let code1 = 4;
+let code2 = 18;
+let code3 = 50;
+code1 = code1 * 25
+code1 = code1 /10
+code2 = code2 + 22
+code3 = code3 - 11
+alert(`The secret code is ${code1} - ${code2} - ${code3}`);
